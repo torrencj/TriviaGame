4d481e1b04dfcf1c71ea3f649f8b4f196323516b
John <john@Mac.local>
2017-08-17 15:12:21 -0500

    finalized design

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,30 +4,105 @@
 var bridgeOfDeath = [
   {
     q:"What is your name?",
-    wa:["Sir Lancelot of Camelot","wrong","wrong","wrong"]
+    a:["Sir Lancelot of Camelot","wrong","wrong","wrong"]
   },
   {
     q:"What is your quest?",
-    wa:["Sir Lancelot of Camelot","wrong","wrong","wrong"]
+    a:["I seek the Grail","wrong","wrong","wrong"]
   },
   {
     q:"What is your favorite color?",
-    wa:["Sir Lancelot of Camelot","wrong","wrong","wrong"]
+    a:["Blue","wrong","wrong","wrong"]
   }
 ]
 
+var bridgeOfDeathGame = {
+
+  welcomeMsg: "STOP! He who would cross the bridge of death must answer me these questions three, ere the other side he see...",
+  failureMsg: "has been cast into the gorge of eternal peril!",
+  interimMsg: "Right, off you go then.",
+  winMsg: "Huh? I don't know that -- AIEEEEEEEE",
+
+  mainData: [
+    {
+      name:"Lancelot",
+      questions: [
+        {
+          q:"What is your name?",
+          a:["Sir Lancelot of Camelot","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is your quest?",
+          a:["To seek the Holy Grail","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is your favorite color?",
+          a:["Blue","wrong","wrong","wrong"]
+        }
+      ]
+    },
+    {
+      name:"Robin",
+      questions: [
+        {
+          q:"What is your name?",
+          a:["Sir Robin of Camelot","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is your quest?",
+          a:["To see the Holy Grail","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is the capital of Assyria?",
+          a:["Aššur","I don't know that!","wrong","wrong"]
+        }
+      ]
+    },
+    {
+      name:"Gallahad",
+      questions: [
+        {
+          q:"What is your name?",
+          a:["Sir Gallahad of Camelot","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is your quest?",
+          a:["I seek the Grail","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is your favorite color?",
+          a:["Blue","Blue... no ye-","wrong","wrong"]
+        }
+      ]
+    },
+    {
+      name:"Arthur, King of the Britons",
+      questions: [
+        {
+          q:"What is your name?",
+          a:["Arthur, King of the Britons","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is your quest?",
+          a:["To seek the Holy Grail","wrong","wrong","wrong"]
+        },
+        {
+          q:"What is the air speed velocity of an unladen swallow?",
+          a:["What do you mean? African or European swallow?","wrong","wrong","wrong"]
+        }
+      ]
+    }
+  ]
+}
+
+var numWrong = 0;
 var currentQuestion = 0;
+var currentCharacter = 0;
 var answers;
 
 // functions
 
-function loadGame(game) {
-
-}
-
 function randomize(array) {
-
-  // var array = [];
   var myindex = 0;
   var temp;
   for (var i = 0; i < array.length; i++) {
@@ -64,70 +139,140 @@ function random(max) {
 
 function done() {
   console.log("we're done!");
-  $("#q-1").html("<h1> We're done!</h1>")
+  currentQuestion = 0;
   $(".answer-wrapper").css("display", "none");
+
+  if (currentCharacter < bridgeOfDeathGame.mainData.length - 1) {
+    $("#q-1 h1").remove();
+    $("#q-1").append("<h3>"+bridgeOfDeathGame.interimMsg+"</h3>")
+    $("#game-image").attr("src","assets/images/meeting.png")
+    currentCharacter++;
+    setTimeout(nextquestion, 3000)
+  } else {
+    $("#q-1").html("<h1>We're done!</h1>")
+
+    showFinal();
+
+    // setTimeout(reset, 5000);
+  }
+
+}
+
+function showFinal() {
+  $("#stat-screen").css("display","block");
+  $("#stat-screen").append("<h3>Wrong Answers: "+numWrong+"</h3>")
+
+  if (numWrong == 0) {
+    $("#game-image").attr("src","assets/images/party.gif")
+
+  } else {
+    $("#game-image").attr("src","assets/images/sad.gif")
+
+  }
 
 }
 
 function reset() {
+  $("#game-image").attr("src","assets/images/meeting.png")
+  $("#stat-screen").css("display","none");
+  $("#stat-screen").empty();
+
   currentQuestion = 0;
+  currentCharacter = 0;
   $("#q-start").css("display", "block");
   $("#q-1").css("display", "none");
 }
 
 function nextquestion() {
-  // currentQuestion++;
-  if (currentQuestion >= bridgeOfDeath.length) {
-    reset();
-  } else {
-    answers = makeAnswerKey(bridgeOfDeath[currentQuestion].wa.length);
-    for (var i = 0; i < answers.length; i++) {
-      $("#answer"+(i+1)).text(bridgeOfDeath[currentQuestion].wa[answers[i]]);
-    }
+  $("#game-image").attr("src","assets/images/oldman.jpg")
 
-    $("#q-1").html("<h1>" + bridgeOfDeath[currentQuestion].q + "</h1>")
-    $("#q-1").css("display", "block");
-    $(".answer-wrapper").css("display", "grid");
+  if (currentCharacter >= bridgeOfDeathGame.mainData.length) {
+    done();
+  } else {
+    if (currentQuestion >= bridgeOfDeathGame.mainData[currentCharacter].questions.length) {
+      done();
+    } else {
+      answers = makeAnswerKey(bridgeOfDeathGame
+                              .mainData[currentCharacter]
+                              .questions[0]
+                              .a
+                              .length);
+
+      for (var i = 0; i < answers.length; i++) {
+        console.log(currentQuestion);
+        $("#answer"+(i+1)).text(bridgeOfDeathGame
+                                .mainData[currentCharacter]
+                                .questions[currentQuestion]
+                                .a[answers[i]]);
+
+      }
+
+      $("#q-1").html("<h1>" + bridgeOfDeathGame
+                              .mainData[currentCharacter]
+                              .questions[currentQuestion]
+                              .q
+                            + "</h1>");
+
+      $("#q-1").css("display", "block");
+      $(".answer-wrapper").css("display", "grid");
+
+      currentQuestion++;
+    }
   }
-  currentQuestion++;
 }
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    //generate HTML
 
-    //add event/clickhandlers/listeners/etc
+    //event/clickhandlers/listeners/etc
     $("#start-button").click(function(){
-
       $("#q-start").css("display", "none");
-
       nextquestion();
-      // var answers = randomize();
+    })
 
+    $("#reset-button").click(function() {
+      reset();
     })
 
     $(".answerbutton").click(function() {
-      // console.log(currentQuestion);
 
-    if (currentQuestion <= (bridgeOfDeath.length - 1)) {
-      if ($(this).text() === bridgeOfDeath[currentQuestion].wa[0]) {
-        console.log("Winner");
-      } else {
-        console.log("loser");
+      if (currentQuestion <= (bridgeOfDeathGame.mainData.length)) {
+        if ($(this).text() === bridgeOfDeathGame
+                                .mainData[currentCharacter]
+                                .questions[currentQuestion - 1]
+                                .a[0]) {
+
+          console.log("Winner");
+
+          $("#q-1").html(
+            "<h1>"
+            + "Correct, next question."
+            + "</h1>"
+          );
+
+        } else {
+          console.log("loser");
+          numWrong++;
+
+          $("#game-image").attr("src","assets/images/death.png")
+
+          $("#q-1").html(
+            "<h1>"
+            + bridgeOfDeathGame.mainData[currentCharacter].name
+            + " "
+            + bridgeOfDeathGame.failureMsg
+            + "</h1>"
+          );
+
+          currentCharacter++;
+          currentQuestion = 0;
+        }
+
+        $(".answer-wrapper").css("display", "none");
+        setTimeout(nextquestion, 1000);
+
       }
-      nextquestion();
-    } else {
-      done();
-    }
 
-      // currentQuestion++;
-      // console.log(currentQuestion);
-
-    })
-
-    //initialize vars, etc
-
-    //go!
-
+    });
 
 }); //document ready function
